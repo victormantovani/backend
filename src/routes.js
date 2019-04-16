0268e9f23ff45e8e312b4599ce87c861216c2a04
Mantovs <victor.mantovani@iv2.com.br>
2019-04-16 01:19:49 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const BoxController = require("./controller/BoxController");
 //     return res.send("Hellow Mantovs1");
 // })
 
-// routes.post("/boxes", BoxController.store);
+routes.post("/boxes", BoxController.store);
 // routes.get("/boxes/:id", BoxController.show);
 // routes.post("/boxes/:id/files",     
 //     multer(multerConfig).single('file'),
