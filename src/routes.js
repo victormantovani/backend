c021d3ad452840fe68e820a35f69ed7a718325a1
Mantovs <victor.mantovani@iv2.com.br>
2019-04-16 01:18:38 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const multerConfig = require('./config/multer');
 
 const  routes = express.Router();
 
-// const BoxController = require("./controller/BoxController");
+const BoxController = require("./controller/BoxController");
 // const FileController = require("./controller/FileController");
 
 // routes.get("/mantovs", (req, res) => {
