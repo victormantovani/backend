492ecd44f727ba362d6dcfeff46238abe6743bd0
Mantovs <victor.mantovani@iv2.com.br>
2019-04-16 01:17:36 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,19 @@ const multerConfig = require('./config/multer');
 
 const  routes = express.Router();
 
-const BoxController = require("./controller/BoxController");
-const FileController = require("./controller/FileController");
+// const BoxController = require("./controller/BoxController");
+// const FileController = require("./controller/FileController");
 
 // routes.get("/mantovs", (req, res) => {
 //     return res.send("Hellow Mantovs1");
 // })
 
-routes.post("/boxes", BoxController.store);
-routes.get("/boxes/:id", BoxController.show);
-routes.post("/boxes/:id/files",     
-    multer(multerConfig).single('file'),
-    FileController.store
-);
+// routes.post("/boxes", BoxController.store);
+// routes.get("/boxes/:id", BoxController.show);
+// routes.post("/boxes/:id/files",     
+//     multer(multerConfig).single('file'),
+//     FileController.store
+// );
 // routes.get("/files", FileController.show);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -31,6 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname,'..','tmp')));
 
-server.use(require("./routes"));//
+app.use(require("./routes"));//
 
 server.listen(process.env.PORT || "3333");
